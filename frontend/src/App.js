2d9fd6a9d056cde6ec9805f9c61f75a983f58a53
safwen-code <safwendjebbi1234@gmail.com>
2024-12-12 20:51:42 +0100

    add register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import react from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './layout/Navbar'
+import Register from './Auth/Register'
+
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+      </Router>
     </>
   )
 }
