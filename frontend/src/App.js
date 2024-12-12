333e119d723ee95366917e326fbd2b7205395954
safwen-code <safwendjebbi1234@gmail.com>
2024-12-12 20:58:40 +0100

    update App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './layout/Navbar'
 import Register from './Auth/Register'
+import Login from './Auth/Login'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </main>
       </Router>
