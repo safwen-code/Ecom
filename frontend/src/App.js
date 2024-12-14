0df23f98f30e0c4b4bf2e3d306f642ff64204b00
safwen-code <safwendjebbi1234@gmail.com>
2024-12-14 18:27:02 +0100

    update prodcut id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './layout/Navbar'
 import Register from './Auth/Register'
 import Login from './Auth/Login'
+import ListProducts from './screens/ListProducts'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/" element={<ListProducts />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
           </Routes>
         </main>
       </Router>
