8fd3a01ad463019745cca845f0da8df4bc88b855
safwen-code <safwendjebbi1234@gmail.com>
2024-12-16 18:18:45 +0100

    Update App finish admin part routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import Register from './Auth/Register'
 import Login from './Auth/Login'
 import ListProducts from './screens/ListProducts'
 import ProductScreen from './screens/ProductScreen'
+import OrderListScreen from './Admin/OrderListScreen'
+import OrderScreen from './Admin/OrderScreen'
+import ProductListScreen from './Admin/ProductListScreen'
+import ProductEditeScreen from './Admin/ProductEditScreen'
+import UserListScreen from './Admin/UserListScreen'
+import UserEditScreen from './Admin/UserEditScreen'
 
 const App = () => {
   return (
@@ -17,6 +23,19 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<ListProducts />} />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/admin/orderlist" element={<OrderListScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
+            <Route
+              path="/admin/productlist"
+              element={<ProductListScreen />}
+              exact
+            />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditeScreen />}
+            />
+            <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
           </Routes>
         </main>
       </Router>
