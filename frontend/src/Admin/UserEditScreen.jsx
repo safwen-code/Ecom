4002d3663ce9cc073ac58089b7fbd20f96030af8
safwen-code <safwendjebbi1234@gmail.com>
2024-12-16 18:18:10 +0100

    Edit  User Screen

--- frontend/src/Admin/UserEditScreen.jsx
+++ frontend/src/Admin/UserEditScreen.jsx
@@ -1,7 +1,119 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Message from '../layout/Message'
+import Loader from '../layout/Loader'
+import {
+  Container,
+  TextField,
+  Typography,
+  Box,
+  Button,
+  FormControlLabel,
+  Checkbox,
+} from '@mui/material'
+
+import { users } from '../utils/data'
 
 const UserEditScreen = () => {
-  return <div>UserEditScreen</div>
+  const { id } = useParams()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
+
+  const loading = false
+  const error = false
+  const user = users.find((user) => user._id === id)
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name)
+      setEmail(user.email)
+      setIsAdmin(user.isAdmin)
+    }
+  }, [user])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    //update user
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Button
+        component={Link}
+        to="/admin/userList"
+        variant="contained"
+        color="primary"
+      >
+        Go Back
+      </Button>
+      <Box
+        component="form"
+        onSubmit={submitHandler}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Edit User
+        </Typography>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="error">{error}</Message>
+        ) : user ? (
+          <>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={isAdmin}
+                  onChange={(e) => setIsAdmin(e.target.checked)}
+                  color="primary"
+                />
+              }
+              label="Admin"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Edit User
+            </Button>
+          </>
+        ) : (
+          <Message variant="error">User not found</Message>
+        )}
+      </Box>
+    </Container>
+  )
 }
 
 export default UserEditScreen
