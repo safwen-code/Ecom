3b738605edd5fe699547f20696a7b3c1299d5edd
safwen-code <safwendjebbi1234@gmail.com>
2024-12-19 00:03:31 +0100

    update cart sceen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
 import {
   Box,
@@ -18,6 +19,7 @@ import {
   Button,
 } from '@mui/material'
 import { Link } from 'react-router-dom'
+
 const CartScreen = () => {
   const removeFromCartHandler = (id) => {}
   const checkoutHandler = () => {}
@@ -72,134 +74,145 @@ const CartScreen = () => {
           <Grid container spacing={3}>
             {/* Cart items list */}
             <Grid item xs={12} md={8}>
-              <Box
-                sx={{
-                  p: 2,
-                  border: '1px solid #ddd',
-                  borderRadius: '8px',
-                  boxShadow: 2,
-                  backgroundColor: 'white',
-                  overflowX: 'auto', // Added to handle overflow for smaller screens
-                }}
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
               >
-                <List>
-                  {cartItems.map((item) => (
-                    <ListItem
-                      key={item.product}
-                      alignItems="flex-start"
-                      divider
-                      sx={{ py: 1 }}
-                    >
-                      <ListItemAvatar>
-                        <Avatar
-                          src={item.image}
-                          alt={item.name}
-                          variant="rounded"
-                          sx={{ width: 60, height: 60 }}
+                <Box
+                  sx={{
+                    p: 2,
+                    border: '1px solid #ddd',
+                    borderRadius: '8px',
+                    boxShadow: 2,
+                    backgroundColor: 'white',
+                    overflowX: 'auto', // Added to handle overflow for smaller screens
+                  }}
+                >
+                  <List>
+                    {cartItems.map((item) => (
+                      <ListItem
+                        key={item.product}
+                        alignItems="flex-start"
+                        divider
+                        sx={{ py: 1 }}
+                      >
+                        <ListItemAvatar>
+                          <Avatar
+                            src={item.image}
+                            alt={item.name}
+                            variant="rounded"
+                            sx={{ width: 60, height: 60 }}
+                          />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={
+                            <Link
+                              to={`/product/${item.product}`}
+                              style={{
+                                textDecoration: 'none',
+                                color: 'inherit',
+                              }}
+                            >
+                              <Typography variant="body1" noWrap>
+                                {item.name}
+                              </Typography>
+                            </Link>
+                          }
+                          secondary={`$ ${item.price}`}
+                          sx={{ ml: 2 }}
                         />
-                      </ListItemAvatar>
-                      <ListItemText
-                        primary={
-                          <Link
-                            to={`/product/${item.product}`}
-                            style={{ textDecoration: 'none', color: 'inherit' }}
-                          >
-                            <Typography variant="body1" noWrap>
-                              {item.name}
-                            </Typography>
-                          </Link>
-                        }
-                        secondary={`$ ${item.price}`}
-                        sx={{ ml: 2 }}
-                      />
-                      <FormControl
-                        variant="outlined"
-                        sx={{
-                          mx: 1,
-                          minWidth: 80,
-                          maxWidth: '100%', // Ensure it adapts to available space
-                          '& .MuiSelect-select': {
-                            py: 0.5,
-                            fontSize: '14px',
-                          },
-                          '& .MuiOutlinedInput-root': {
-                            borderRadius: '4px',
-                          },
-                          '& .MuiSelect-icon': {
-                            color: 'black',
-                          },
-                          '@media (max-width:600px)': {
-                            minWidth: 60,
+                        <FormControl
+                          variant="outlined"
+                          sx={{
+                            mx: 1,
+                            minWidth: 80,
+                            maxWidth: '100%', // Ensure it adapts to available space
                             '& .MuiSelect-select': {
-                              fontSize: '12px',
-                              py: '4px',
+                              py: 0.5,
+                              fontSize: '14px',
                             },
                             '& .MuiOutlinedInput-root': {
-                              fontSize: '12px',
+                              borderRadius: '4px',
                             },
-                          },
-                        }}
-                      >
-                        <Select
-                          value={item.qty}
-                          //   onChange={(e) => {
-                          //     dispatch(
-                          //       addToCart(item.product, Number(e.target.value)),
-                          //     )
-                          //   }}
+                            '& .MuiSelect-icon': {
+                              color: 'black',
+                            },
+                            '@media (max-width:600px)': {
+                              minWidth: 60,
+                              '& .MuiSelect-select': {
+                                fontSize: '12px',
+                                py: '4px',
+                              },
+                              '& .MuiOutlinedInput-root': {
+                                fontSize: '12px',
+                              },
+                            },
+                          }}
                         >
-                          {[...Array(item.countInStock).keys()].map((x) => (
-                            <MenuItem key={x + 1} value={x + 1}>
-                              {x + 1}
-                            </MenuItem>
-                          ))}
-                        </Select>
-                      </FormControl>
-                      <IconButton
-                        onClick={() => removeFromCartHandler(item.product)}
-                        sx={{ color: 'red' }}
-                      >
-                        <DeleteOutlineIcon />
-                      </IconButton>
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
+                          <Select value={item.qty}>
+                            {[...Array(item.countInStock).keys()].map((x) => (
+                              <MenuItem key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl>
+                        <IconButton
+                          onClick={() => removeFromCartHandler(item.product)}
+                          sx={{ color: 'red' }}
+                        >
+                          <DeleteOutlineIcon />
+                        </IconButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                </Box>
+              </motion.div>
             </Grid>
 
             {/* Cart summary */}
             <Grid item xs={12} md={4}>
-              <Card sx={{ borderRadius: '8px', boxShadow: 2 }}>
-                <CardContent>
-                  <List>
-                    <ListItem>
-                      <Typography variant="h6">
-                        SubTotal{' '}
-                        {cartItems.reduce((acc, item) => acc + item.qty, 0)}{' '}
-                        Items
-                      </Typography>
-                    </ListItem>
-                    <ListItem>
-                      <Typography variant="body1">
-                        $
-                        {cartItems
-                          .reduce((acc, item) => acc + item.price * item.qty, 0)
-                          .toFixed(2)}
-                      </Typography>
-                    </ListItem>
-                    <ListItem>
-                      <Button
-                        variant="contained"
-                        fullWidth
-                        disabled={cartItems.length === 0}
-                        onClick={checkoutHandler}
-                      >
-                        Proceed To Checkout
-                      </Button>
-                    </ListItem>
-                  </List>
-                </CardContent>
-              </Card>
+              <motion.div
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Card sx={{ borderRadius: '8px', boxShadow: 2 }}>
+                  <CardContent>
+                    <List>
+                      <ListItem>
+                        <Typography variant="h6">
+                          SubTotal{' '}
+                          {cartItems.reduce((acc, item) => acc + item.qty, 0)}{' '}
+                          Items
+                        </Typography>
+                      </ListItem>
+                      <ListItem>
+                        <Typography variant="body1">
+                          $
+                          {cartItems
+                            .reduce(
+                              (acc, item) => acc + item.price * item.qty,
+                              0,
+                            )
+                            .toFixed(2)}
+                        </Typography>
+                      </ListItem>
+                      <ListItem>
+                        <Button
+                          variant="contained"
+                          fullWidth
+                          disabled={cartItems.length === 0}
+                          onClick={checkoutHandler}
+                        >
+                          Proceed To Checkout
+                        </Button>
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                </Card>
+              </motion.div>
             </Grid>
           </Grid>
         </Box>
