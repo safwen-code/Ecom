bedaf3bac4bd6762b0e0c1dc388315ffd418744a
safwen-code <safwendjebbi1234@gmail.com>
2024-12-22 20:52:46 +0100

    add prd by id action

--- frontend/src/Actions/prdActions.js
+++ frontend/src/Actions/prdActions.js
@@ -1,10 +1,14 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from '../Constants/prdConstants'
 import { products } from '../utils/data'
 
+//all prd
 export const diplayProducts = () => async (dispatch) => {
   dispatch({ type: PRODUCT_LIST_REQUEST })
   try {
@@ -15,6 +19,24 @@ export const diplayProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload: 'no data',
+    })
+  }
+}
+
+//prd by id
+export const displayProductby = (id) => async (dispatch) => {
+  console.log(id)
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    const prd = products.find((el) => el._id === id)
+    console.log(prd)
+    if (prd) {
+      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: prd })
+    }
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: 'no data',
     })
   }
