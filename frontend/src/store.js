bef0ad4c1c1bdc24d4edcaf4ee67b7b2b4cc6229
safwen-code <safwendjebbi1234@gmail.com>
2024-12-22 21:57:53 +0100

    add login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,14 @@ import { composeWithDevTools } from '@redux-devtools/extension'
 
 //import reducers
 import { prdDetailsReducer, prdListReducer } from './Reducers/prdReducers'
+import { loginReducer } from './Reducers/cltReducers'
 
 //create Reducers
 const reducer = combineReducers({
   //add reducers here exp
   productList: prdListReducer,
   productID: prdDetailsReducer,
+  cltLogin: loginReducer,
 })
 
 //create some storage data
