774aaddeecca3af13f65c2ad3b75fdf6871f86a6
safwen-code <safwendjebbi1234@gmail.com>
2024-12-22 20:56:46 +0100

    add prd by id rds

--- frontend/src/Reducers/prdReducers.js
+++ frontend/src/Reducers/prdReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -14,6 +17,23 @@ export const prdListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: payload }
 
+    default:
+      return state
+  }
+}
+
+export const prdDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action,
+) => {
+  const { type, payload } = action
+  switch (type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: false, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: payload }
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: payload }
     default:
       return state
   }
