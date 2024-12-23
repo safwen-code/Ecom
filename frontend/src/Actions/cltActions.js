b1e350090b2e72b518a4adf49bbda4a32cefc245
safwen-code <safwendjebbi1234@gmail.com>
2024-12-23 17:59:33 +0100

    add register

--- frontend/src/Actions/cltActions.js
+++ frontend/src/Actions/cltActions.js
@@ -2,6 +2,9 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../Constants/cltConstants'
 import { users } from '../utils/data'
 
@@ -20,5 +23,18 @@ export const LoginUser = (email, password) => async (dispatch) => {
     }
   } catch (error) {
     dispatch({ type: USER_LOGIN_FAIL, payload: error.message })
+  }
+}
+
+export const RegisterUser = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST })
+    const user = { name, email, password }
+    if (user) {
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: user })
+      localStorage.setItem('userInfo', JSON.stringify(user))
+    }
+  } catch (error) {
+    dispatch({ type: USER_REGISTER_FAIL, payload: error.message })
   }
 }
