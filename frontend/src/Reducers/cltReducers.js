234e209b9163ba8c87605c4d24f768cdeaa8e8a5
safwen-code <safwendjebbi1234@gmail.com>
2024-12-23 18:01:05 +0100

    add register reducer

--- frontend/src/Reducers/cltReducers.js
+++ frontend/src/Reducers/cltReducers.js
@@ -2,17 +2,34 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../Constants/cltConstants'
 
-export const loginReducer = async (state = {}, action) => {
+export const loginReducer = (state = {}, action) => {
   const { type } = action
   switch (type) {
     case USER_LOGIN_REQUEST:
-      return { loading: false, ...state }
+      return { loading: true, ...state }
     case USER_LOGIN_SUCCESS:
       return { loading: false, userInfo: action.payload }
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const registerReducer = (state = {}, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true, ...state }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: payload }
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: payload }
     default:
       return state
   }
