524d5dad7efab4ff52cb02e602651add695d9334
safwen-code <safwendjebbi1234@gmail.com>
2025-01-13 18:08:39 +0100

    create crud exemple

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,43 @@
 const express = require('express')
-const { Client } = require('pg')
 
 const app = express()
+const { ConnectionDb, pool } = require('./connectdb')
 
-// ********** Connect to DB (pgAdmin) **********
-const connectdb = new Client({
-  user: 'postgres',
-  password: 'pascal',
-  host: 'localhost',
-  database: 'Ecom',
-  port: 5432,
-})
-
-// Establish connection to the database
-connectdb
-  .connect()
-  .then(() => {
-    console.log('Connected to PostgreSQL database')
-  })
-  .catch((err) => {
-    console.error('Failed to connect to database', err)
-    process.exit(1) // Exit the process if DB connection fails
-  })
+//cnt to db
+ConnectionDb()
 
 // ********** Setup Routes **********
-app.get('/home', (req, res) => {
+app.get('/home', async (req, res) => {
+  let test = 'SELECT * FROM "user" '
+  const resut = await pool.query(test)
+  console.log(resut.rows)
   res.send('<h1>Hello</h1>') // Display "hello" in an HTML heading
+})
+
+app.post('/add', async (req, res) => {
+  const insert = `
+  INSERT INTO "user" (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+`
+  const values = ['ahmed', 'ahmed@gmail.com', 'pascal.html']
+  const rest = await pool.query(insert, values)
+  console.log(rest)
+})
+
+app.delete('/delete/:id', async (req, res) => {
+  let id = req.params.id
+  const del = `delete from "user" where id = ${id}`
+  const reslt = await pool.query(del)
+  console.log(reslt)
+})
+
+app.put('/upd/:id', async (req, res) => {
+  let id = req.params.id
+  const upd = `UPDATE "user" SET name=$1, email=$2, password=$3 where id = ${id};`
+  const vls = ['siwar', 'siwar@gmail.com', '123456']
+  const reslt = await pool.query(upd, vls)
+  console.log(reslt.rows)
 })
 
 // ********** Start Server **********
