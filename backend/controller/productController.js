c742d9287b30956c30db5e4a965c40caad9fce9e
safwen-code <safwendjebbi1234@gmail.com>
2025-01-16 23:00:23 +0100

    add product v1

---
+++ backend/controller/productController.js
@@ -0,0 +1,71 @@
+const asynchandler = require('express-async-handler')
+const { pool } = require('../connectdb')
+
+// Add product
+const addProduct = asynchandler(async (req, res) => {
+  try {
+    // Ensure req.user._id exists and is valid
+    if (!req.user || !req.user.id) {
+      return res.status(400).json({ message: 'User not authenticated' })
+    }
+
+    // Extract and validate user ID
+    const user_id = parseInt(req.user.id, 10)
+    if (isNaN(user_id)) {
+      return res.status(400).json({ message: 'Invalid user ID' })
+    }
+
+    // Extract product details from the request body
+    const {
+      name,
+      price,
+      image,
+      category,
+      countInStock,
+      numReviews,
+      description,
+    } = req.body
+
+    // Validate required fields
+    if (!name || !price || !image || !category || !description) {
+      return res
+        .status(400)
+        .json({ message: 'Missing required fields in the request body' })
+    }
+
+    // Prepare values for the query
+    const vls = [
+      name,
+      user_id,
+      image,
+      category,
+      countInStock || 0, // Default to 0 if not provided
+      numReviews || 0, // Default to 0 if not provided
+      price,
+      description,
+    ]
+
+    // SQL query
+    const query = `
+      INSERT INTO "products" 
+      (name, user_id, image, category, count_in_stock, num_reviews, price, description) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+      RETURNING *;
+    `
+
+    // Execute the query
+    const result = await pool.query(query, vls)
+
+    // Check result and respond
+    if (result.rowCount === 0) {
+      return res.status(400).json({ message: 'Product could not be added' })
+    }
+
+    res.status(200).json(result.rows[0]) // Return the inserted row
+  } catch (error) {
+    console.error('Error adding product:', error)
+    res.status(500).json({ message: 'Server error' })
+  }
+})
+
+module.exports = { addProduct }
