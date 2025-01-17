547a0e5997728f5463d0030d1c77209943e51ff4
safwen-code <safwendjebbi1234@gmail.com>
2025-01-17 17:05:49 +0100

    update products route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,8 +4,8 @@ const router = express.Router()
 
 const { addProduct } = require('../controller/productController')
 const protect = require('../middelware/authMiddel')
-
+const upload = require('../middelware/uploadMiddel')
 //add product
-router.post('/addproduct', protect, addProduct)
+router.post('/addproduct', protect, upload.single('image'), addProduct)
 
 module.exports = router
